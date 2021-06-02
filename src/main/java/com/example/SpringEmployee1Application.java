c51d1be7bbfa7338e65f9b51da3d16e2b71f304a
VineethaGarre <84893543+VineethaGarre@users.noreply.github.com>
2021-06-02 09:35:50 +0530

    Update SpringEmployee1Application.java

--- src/main/java/com/example/SpringEmployee1Application.java
+++ src/main/java/com/example/SpringEmployee1Application.java
@@ -3,11 +3,24 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 
+    import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+ 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 @SpringBootConfiguration
-public class SpringEmployee1Application {
+public class SpringEmployee1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmployee1Application.class, args);
 	}
+	@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+ 
+return application.sources(SpringEmployeeApplication.class);
+
+ 
+}
 
 }
