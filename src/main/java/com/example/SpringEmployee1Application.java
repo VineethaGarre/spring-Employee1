a80da9d49cb78d6348ff6b5e1dbed839fd337fef
Vineetha <you@example.com>
2021-06-02 09:13:50 +0530

    done

---
+++ src/main/java/com/example/SpringEmployee1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class SpringEmployee1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployee1Application.class, args);
+	}
+
+}
